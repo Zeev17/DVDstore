13649c56e69303f455415799db2f78535ebc1d07
zeev <zeev@zeev.com>
2022-04-20 17:50:24 +0200

    setter-getter Movie

---
+++ src/main/java/com/mycompany/dvdstore/repository/MovieRepository.java
@@ -0,0 +1,13 @@
+package com.mycompany.dvdstore.repository;
+
+import com.mycompany.dvdstore.entity.Movie;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MovieRepository {
+
+    private List<Movie> movies = new ArrayList<>();
+
+
+}
---
+++ src/main/java/com/mycompany/dvdstore/service/MovieService.java
@@ -0,0 +1,4 @@
+package com.mycompany.dvdstore.service;
+
+public class MovieService {
+}
